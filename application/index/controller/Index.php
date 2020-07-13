ccdb5a5ca8133cd1e286044ead09bd6d19b3a8e7
guangtang <guangtang@qingmutec.com>
2020-07-13 20:29:24 +0800

    test

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -23,4 +23,23 @@ class Index extends Base
         ]);
     }
 
+    //测试用php连接socket
+    public function test()
+    {
+        $st="socket send message";
+        $length = strlen($st);
+        //创建tcp套接字
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        //连接tcp
+        socket_connect($socket, '127.0.0.1',8081);
+        //向打开的套集字写入数据（发送数据）
+        $s = socket_write($socket, $st, $length);
+        //从套接字中获取服务器发送来的数据
+        $msg = socket_read($socket,8190);
+
+        echo $msg;
+        //关闭连接
+        socket_close($socket);
+    }
+
 }
